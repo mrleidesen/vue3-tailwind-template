2a90fe7b9e613bff292e1975ccc9deba8152bd01
mrleidesen <mrleidesen@outlook.com>
2022-01-29 10:01:39 +0800

    chore: format code

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '@/views/Home.vue'
+import { createRouter, createWebHashHistory } from "vue-router";
+import Home from "@/views/Home.vue";
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes: [
     {
-      path: '/',
-      component: Home
-    }
-  ]
-})
+      path: "/",
+      component: Home,
+    },
+  ],
+});
 
-export default router
+export default router;
