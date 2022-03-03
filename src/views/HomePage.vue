2022addc2988b8ec0f5bdede97054b40e5fe78b6
mrleidesen <mrleidesen@outlook.com>
2022-03-03 09:23:47 +0800

    feat: add typescript

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,3 +1,24 @@
 <template>
   <h1 class="text-red-500 text-[20px]">Hello Home!</h1>
+  <p>{{ count }}</p>
+  <button @click="increment">increment</button>
 </template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  setup() {
+    const count = ref<number>(0);
+
+    const increment = () => {
+      count.value++;
+    };
+
+    return {
+      count,
+      increment,
+    };
+  },
+});
+</script>
