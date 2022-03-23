0edeec41c38c87f941be03130613ff3ae18f7294
mrleidesen <mrleidesen@outlook.com>
2022-03-23 12:43:07 +0800

    fix: eslint config

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,7 +12,8 @@
   <button @click="state.count++">count is: {{ state.count }}</button>
   <p>
     Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
+    <code>components/HelloWorld.vue</code>
+    to test hot module replacement.
   </p>
 </template>
 
